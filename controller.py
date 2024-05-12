5ac3a02b72967bbdde1c4543a582884ff89478d1
Rafal Mielowski <mielex@gmail.com>
2024-05-12 23:55:58 +0200

    improvements to python

--- controller.py
+++ controller.py
@@ -1,7 +1,7 @@
 import serial
-from enum import Enum
-import threading
+from threading import Thread, current_thread
 import logging
+from queue import Queue
 
 Lift_Down     = 0b00000001
 Lift_Middle_1 = 0b00000010
@@ -9,7 +9,7 @@ Lift_Middle_2 = 0b00000100
 Lift_Middle_3 = 0b00001000
 Lift_Middle_4 = 0b00010000
 Lift_Middle_5 = 0b00100000
-Lift_Up       = 0b01000000
+Lift_Top       = 0b01000000
 
 Cart_RideStart  = 0b00000001
 Cart_RideFinish = 0b00000010
@@ -20,12 +20,16 @@ Command_Stop      = b'\x00'
 Command_Forward   = b'\x01'
 Command_Backward  = b'\x02'
 
-class Controller:
+class Controller(Thread):
   connection = serial.Serial('/dev/ttyUSB0')
   connection.close()
 
+  action_queue = Queue()
+
   last_lift_state = 0b00000000
   last_cart_state = 0b00000000
+  motor_state = Command_Stop
+  motor_speed = 0
 
   on_lift_down = None
   on_lift_middle_1 = None
@@ -33,7 +37,7 @@ class Controller:
   on_lift_middle_3 = None
   on_lift_middle_4 = None
   on_lift_middle_5 = None
-  on_lift_up = None
+  on_lift_top = None
 
   on_cart_ride_start = None
   on_cart_ride_finish = None
@@ -42,79 +46,105 @@ class Controller:
 
   is_avaliable = False
 
-  def __init__(self):
-    self.thread = threading.Thread(name="Controller thread", target=self.loop)
-
   def start(self):
-    self.thread.start()
+    super().start()
+    while not self.is_avaliable:
+      pass
 
-  def wait(self):
-    self.thread.join()
 
   def stop(self):
-    self.motor_stop()
-    self.is_avaliable = False
-    self.connection.close()
+    self.action_queue.put({ "key": "exit" })
+    if current_thread().name != self.name:
+      self.join()
 
   def motor_stop(self):
-    self.connection.write(Command_Stop)
+    self.action_queue.put({ "key": "stop" })
 
   def motor_forward(self, speed: int):
-    self.connection.write(Command_Forward)
-    self.connection.write(speed.to_bytes(1, 'little'))
+    self.action_queue.put({ "key": "forward", "value": speed })
 
   def motor_backward(self, speed: int):
-    self.connection.write(Command_Backward)
-    self.connection.write(speed.to_bytes(1, 'little'))
+    self.action_queue.put({ "key": "backward", "value": speed })
 
   def fire_lift_events(self, state):
     changed_lift_state = self.last_lift_state ^ state
+    logging.debug("{}, {}, {}".format(self.last_lift_state, state, changed_lift_state))
 
-    if changed_lift_state & Lift_Down > 0 and self.on_lift_down is not None:
-      self.on_lift_down(True if state & Lift_Down > 0 else False)
-    if changed_lift_state & Lift_Middle_1 > 0 and self.on_lift_middle_1 is not None:
-      self.on_lift_middle_1(True if state & Lift_Middle_1 > 0 else False)
-    if changed_lift_state & Lift_Middle_2 > 0 and self.on_lift_middle_2 is not None:
-      self.on_lift_middle_2(True if state & Lift_Middle_2 > 0 else False)
-    if changed_lift_state & Lift_Middle_3 > 0 and self.on_lift_middle_3 is not None:
-      self.on_lift_middle_3(True if state & Lift_Middle_3 > 0 else False)
-    if changed_lift_state & Lift_Middle_4 > 0 and self.on_lift_middle_4 is not None:
-      self.on_lift_middle_4(True if state & Lift_Middle_4 > 0 else False)
-    if changed_lift_state & Lift_Middle_5 > 0 and self.on_lift_middle_5 is not None:
-      self.on_lift_middle_5(True if state & Lift_Middle_5 > 0 else False)
-    if changed_lift_state & Lift_Up > 0 and self.on_lift_up is not None:
-      self.on_lift_up(True if state & Lift_Up > 0 else False)
+    def process(flag, action):
+      if changed_lift_state & flag > 0:
+        value = True if state & flag > 0 else False
+        logger.debug("Lift {} flag chage {} -> {}",flag, not value, value)
+        if action is not None:
+          action(value)
+
+    process(Lift_Down, self.on_lift_down,)
+    process(Lift_Middle_1, self.on_lift_middle_1)
+    process(Lift_Middle_2, self.on_lift_middle_2)
+    process(Lift_Middle_3, self.on_lift_middle_3)
+    process(Lift_Middle_4, self.on_lift_middle_4)
+    process(Lift_Middle_5, self.on_lift_middle_5)
+    process(Lift_Top, self.on_lift_top)
 
     self.last_lift_state = state
 
   def fire_cart_events(self, state):
     changed_cart_state = self.last_cart_state ^ state
 
-    if changed_cart_state & Cart_RideStart > 0 and self.on_cart_ride_start is not None:
-      self.on_cart_ride_start(True if state & Cart_RideStart > 0 else False)
-    if changed_cart_state & Cart_RideFinish > 0 and self.on_cart_ride_finish is not None:
-      self.on_cart_ride_finish(True if state & Cart_RideFinish > 0 else False)
-    if changed_cart_state & Cart_Station > 0 and self.on_cart_station is not None:
-      self.on_cart_station(True if state & Cart_Station > 0 else False)
-    if changed_cart_state & Cart_Lift > 0 and self.on_cart_lift is not None:
-      self.on_cart_lift(True if state & Cart_Lift > 0 else False)
+    def process(flag, action):
+      if changed_cart_state & flag > 0:
+        value = True if state & flag > 0 else False
+        logger.debug("Cart {} flag chage {} -> {}",flag, not value, value)
+        if action is not None:
+          action(value)
+
+    process(Cart_RideStart, self.on_cart_ride_start)
+    process(Cart_RideFinish, self.on_cart_ride_finish)
+    process(Cart_Station, self.on_cart_station)
+    process(Cart_Lift, self.on_cart_lift)
 
     self.last_cart_state = state
 
-
-  def loop(self):
+  def run(self):
     self.connection.open()
-    self.connection.read(4)
+    state = self.connection.read(4)
+    logging.debug("Initial state {}".format(state))
+    self.last_lift_state = int.from_bytes(state[0:1])
+    self.last_cart_state = int.from_bytes(state[1:2])
+    self.motor_state = int.from_bytes(state[2:3])
+    self.motor_speed = int.from_bytes(state[3:4])
+
     self.is_avaliable = True
     while self.is_avaliable:
       state = self.connection.read(4)
-      # logging.debug("State {}".format(state))
+      logging.debug("New state {}".format(state))
 
       new_lift_state = int.from_bytes(state[0:1])
       new_cart_state = int.from_bytes(state[1:2])
-      motor_state = int.from_bytes(state[2:3])
-      motor_speed = int.from_bytes(state[3:4])
+      self.motor_state = int.from_bytes(state[2:3])
+      self.motor_speed = int.from_bytes(state[3:4])
 
       self.fire_lift_events(new_lift_state)
       self.fire_cart_events(new_cart_state)
+
+      if not self.action_queue.empty():
+        action = self.action_queue.get()
+        key = action["key"]
+        if key == "forward":
+          self.connection.write(Command_Forward)
+          self.connection.write(action["value"].to_bytes(1, 'little'))
+        elif key == "backward":
+          self.connection.write(Command_Backward)
+          self.connection.write(action["value"].to_bytes(1, 'little'))
+        elif key == "stop":
+          self.connection.write(Command_Stop)
+          self.connection.write(b'\x00')
+        elif key == "exit":
+          self.connection.write(Command_Stop)
+          self.connection.write(b'\x00')
+          self.is_avaliable = False
+        else:
+          logging.warning("Unknow action {}", action)
+
       pass
+
+    self.connection.close()
--- lift.py
+++ lift.py
@@ -1,19 +1,20 @@
 from statemachine import StateMachine, State
+from enum import Enum
 
 class LiftStateMachine(StateMachine):
   "Lift state machine"
-  initial=State(initial=True)
+  initial = State(initial=True)
 
-  at_bottom=State()
-  at_middle_1=State()
-  at_middle_2=State()
-  at_middle_3=State()
-  at_middle_4=State()
-  at_middle_5=State()
-  at_top=State()
+  at_bottom = State()
+  at_middle_1 = State()
+  at_middle_2 = State()
+  at_middle_3 = State()
+  at_middle_4 = State()
+  at_middle_5 = State()
+  at_top = State()
 
-  moving_down=State()
-  moving_up=State()
+  moving_up = State()
+  moving_down = State()
 
   initialize_bottom = initial.to(at_bottom)
   initialize_middle_1 = initial.to(at_middle_1)
@@ -34,19 +35,12 @@ class LiftStateMachine(StateMachine):
     | at_middle_5.to(moving_up)
   )
 
-  move_down = (
-    at_top.to(moving_down)
-    | at_middle_5.to(moving_down)
-    | at_middle_4.to(moving_down)
-    | at_middle_3.to(moving_down)
-    | at_middle_2.to(moving_down)
-    | at_middle_1.to(moving_down)
-  )
-
+  move_down = at_top.to(moving_down)
   stop_at_bottom = moving_down.to(at_bottom)
+
+  stop_at_middle_1 = moving_up.to(at_middle_1)
+  stop_at_middle_2 = moving_up.to(at_middle_2)
+  stop_at_middle_3 = moving_up.to(at_middle_3)
+  stop_at_middle_4 = moving_up.to(at_middle_4)
+  stop_at_middle_5 = moving_up.to(at_middle_5)
   stop_on_top = moving_up.to(at_top)
-  stop_at_middle_1 = (moving_up.to(at_middle_1) | moving_down.to(at_middle_1))
-  stop_at_middle_2 = (moving_up.to(at_middle_2) | moving_down.to(at_middle_2))
-  stop_at_middle_3 = (moving_up.to(at_middle_3) | moving_down.to(at_middle_3))
-  stop_at_middle_4 = (moving_up.to(at_middle_4) | moving_down.to(at_middle_4))
-  stop_at_middle_5 = (moving_up.to(at_middle_5) | moving_down.to(at_middle_5))
