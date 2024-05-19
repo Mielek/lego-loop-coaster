d8686dee031e7d20b62046a82dd5c8506d8a1364
Rafal Mielowski <mielex@gmail.com>
2024-05-19 21:51:20 +0200

    start with http server

--- ride_http_server.py
+++ ride_http_server.py
@@ -1,8 +1,10 @@
 import logging
-from datetime import datetime
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
 from queue import Queue
 from schedule import Scheduler
 from controller import Controller
+from rides import setup_controller_events, setup_clock_events, ClockRideStateMachine, SingleRideStateMachine
 
 logging.basicConfig(
   level=logging.INFO,
@@ -16,4 +18,29 @@ scheduler = Scheduler()
 setup_controller_events(controller, events)
 setup_clock_events(scheduler, events)
 
+clock = ClockRideStateMachine(controller)
+single = SingleRideStateMachine(controller)
+
+hostName = "localhost"
+serverPort = 8080
+
+class DrahaController(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/html")
+    self.end_headers()
+  def do_POST(self):
+    pass
+
+
+webServer = HTTPServer((hostName, serverPort), DrahaController)
+logging.info("Server started http://%s:%s" % (hostName, serverPort))
+
+try:
+  webServer.serve_forever()
+except KeyboardInterrupt:
+  pass
+
+webServer.server_close()
+logging.info("Server stopped.")
 
